9414ad3cc136d8a0a7e74eface386db3b887abf0
AntonRadchenko <ipetox@mail.ru>
2025-10-07 15:11:31 +0300

    add tests for internal/store

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,3 +1,37 @@
 package store
 
-func Test
+import (
+	"testing"
+)
+
+// проверяет сразу Set, Get и Del
+// Set - должен корректно их записать
+// Get - должен корректно их вернуть
+func TestStore_SetGetDel(t *testing.T) {
+	// создаём новое хранилище
+	s := NewStore()
+	// проверяем SET
+	s.Set("name", "anton") 
+
+	// проверяем GET
+	val, ok := s.Get("name")
+	// проверяем что ключ доступен
+	if !ok {
+		t.Fatalf("expected key to exist")
+	}
+	if val != "anton" {
+		t.Errorf("expected 'anton', got '%s'", val)
+	}
+
+	// проверяем DEL
+	deleted := s.Del("name")
+	if deleted != 1 {
+		t.Errorf("expected 1 deleted, got %d", deleted)
+	}
+
+	// проверяем что на месте удаленного значения пустота (nil)
+	_, ok = s.Get("name")
+	if ok {
+		t.Errorf("expected key 'name' to be deleted, but still exists")
+	}
+}
---
+++ internal/store/ttl_test.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStore_TTL(t *testing.T) {
+	s := NewStore()
+	// 1) Set() - создаем ключ-значение Expire()
+	// 2) Expire() - устанавливаем на ключ TTL (возвращает false если ключа нет)
+	// 3) Получаем ключ до истечения времени
+	// 4) Получаем ключ после истечения времени (если Get вернет false, то все в порядке)
+	s.Set("name", "anton")
+	ok := s.Expire("name", 1)
+	
+	// проверяем до истечения срока
+	val, ok := s.Get("name")
+	if !ok || val != "anton" {
+		t.Fatalf("expected key to exist before TTL expires")
+	}
+
+	// ждем истечения 
+	time.Sleep(1100*time.Millisecond)
+
+	// проверяем после истечения срока
+	_, ok = s.Get("name") 
+	if ok {
+		t.Errorf("expected key 'anton' to expire")
+	}
+}
